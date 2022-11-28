cc0d063161c43cb14d632719436594a2e08dc7ff
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-11-28 16:02:47 +0600

    category setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 // middlware
@@ -10,6 +11,34 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sabbir.0dgpj5g.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const relaseProductCollaction = client.db('carResale').collection('resaleProduct');
+        
+        app.get('/category', async (req, res) => {
+            
+            const query = {};
+            const category = await relaseProductCollaction.find(query).toArray();
+            res.send(category);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const singelCategory = await relaseProductCollaction.findOne(query);
+            res.send(singelCategory)
+        });   
+    }
+
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));;
 
 
 
